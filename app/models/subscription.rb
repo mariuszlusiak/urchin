2510f44d0a7ffb9e2460d67f269b6d39d169681f
amer <amer.jazaerly@gmail.com>
2010-10-07 18:01:26 +0300

    Add more comments and rename a function name

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,16 +7,19 @@ class Subscription < ActiveRecord::Base
   validates :package, presence:true
   validates :user, presence:true
 
-  def going_messages
+  # Return the number of sent messages for today by this subscription
+  def sent_messages_for_today
    recipients.where('recipients.created_at >= ? AND recipients.subscription_id = ?',
       Date.today.beginning_of_day,id).count
   end
 
+  # Return the day limit number of this subscription
   def day_limit
     package.day_limit
   end
 
+  # Return the rest messages limit for today for this subscription
   def today_limit
-    package.day_limit - going_messages
+    package.day_limit - sent_messages_for_today
   end
 end
