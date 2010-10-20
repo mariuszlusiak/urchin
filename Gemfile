42f6d2eafb735cb671b7b7c9479aa98e77e5db75
amer <amer.jazaerly@gmail.com>
2010-10-20 13:40:58 +0300

    Resque queuing System

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.0'
+gem 'rails', '3.0.1'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -12,11 +12,12 @@ gem 'nifty-generators'
 gem 'delayed_job','2.1.0.pre2'
 gem 'resque','1.11.0' ,:git => 'http://github.com/defunkt/resque.git', :require => 'resque/server'
 gem 'resque-forker'
+
 group :development, :test do
   gem 'rails-erd'
   gem 'ruby-debug19'
 end
-#gem 'delayed_job','2.0.3'
+
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- lib/sms.rb
+++ lib/sms.rb
@@ -1,23 +1,29 @@
 require 'net/http'
 
+# This class would be perform by Resque Workers
 class Msg
   
   # Touch the link :) and add log it
   def self.perform(message_id,sender,host, path, recipient_id)
-    # Create message.log file and message_logger if it wasn't exist before
-    message_logger ||= Logger.new("#{Rails.root}/log/message.log")
     
     # You can rise error here, if you want to keep the message in the queue
-    #raise "Error Message wa wa wa wa hoh hoh oh ho"
+    # raise "Error Message wa wa wa wa hoh hoh oh ho"
     # sending the message by HTTP
     response = Net::HTTP.get_response(host, path)
 
     begin
+      # Create message.log file and message_logger if it wasn't exist before
+      message_logger ||= Logger.new("#{Rails.root}/log/message.log")
+
+      # Get the recipient from the database
       recipient = Recipient.find(recipient_id)
       recipient.response = response.body
       recipient.sent_at = Time.zone.now
       recipient.save
+
+      # Get the message from the database
       message = Message.find(message_id)
+
       # Logging
       log = "Message [#{
       message.id}]:\n\tSender id: #{
@@ -29,9 +35,12 @@ class Msg
       recipient.sent_at}\n\tRespone:\n\t\tBody: #{response.body}\n\t\tCode: #{response.code}\n"
       message_logger.info(log)
 
+      # More logging options
       #message_logger.info("http://"+host+path)
-      #message_logger.info(@message.to_yaml + response.to_yaml + @sender.to_yaml + "\n")# Log recored
+      #message_logger.info(@message.to_yaml + response.to_yaml + @sender.to_yaml + "\n")
+
     rescue Exception => e
+      # Logging Errors
       message_logger.error(e.message + '/n' + e.backtrace.inspect)
     end
   end
@@ -42,7 +51,7 @@ class Sms
   def initialize(message)
     @message = message
     @sender =  @message.sender # add the sender ID
-    send_to_queue
+    enqueue
   end
 
 #  # Create message.log file and message_logger if it wasn't exist before
@@ -54,12 +63,13 @@ class Sms
   def as_unicode
     a=[];@message.text.unpack('U*').each{|c| a << sprintf("%04x",c)};a.join
   end
+
   # Convert Message to URI encoding
   def as_ascii
     CGI::escape(@message.text)
   end
 
-  def send_to_queue
+  def enqueue
     # Host name
     host = 'www.it2sms.com'
 
@@ -67,12 +77,14 @@ class Sms
     @message.ascii ? (msg,mt = as_ascii,0) : (msg,mt = as_unicode,1)
 
     # Create Queue Name
-    queue = 'queue_' + @message.user.id.to_s
+    queue = 'user_' + @message.user.id.to_s
 
     # Iterates on each number in the array
     @message.recipients.each do |recipient|
       path = "/sendsms/sendsms.asp?username=inet&password=inetsyria88212&mno=#{
       recipient.mobile_number}&Msg=#{msg}&sid=#{@sender}&fl=0&mt=#{mt}&ipcl=91.144.8.199"
+
+      # enqueue
       Resque::Job.create(queue, Msg, @message.id,@sender, host, path, recipient.id)
     end
   end
