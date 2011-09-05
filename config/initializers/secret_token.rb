e8422db9bf029ed6360d7d0fcaeb3646da543be0
Amer Jazaerli <amer.jazaerly@gmail.com>
2011-09-05 15:47:50 +0200

    Adding database.yml file :)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Smsee::Application.config.secret_token = '90164c6e555f81879f356290564c53a2de49638a989f3bffcbe15a3269423f5082a23a57593e141b3f888ad16702a77430b6a107521b97d45f1790eea94afb61'
+Urchin::Application.config.secret_token = '90164c6e555f81879f356290564c53a2de49638a989f3bffcbe15a3269423f5082a23a57593e141b3f888ad16702a77430b6a107521b97d45f1790eea94afb61'
